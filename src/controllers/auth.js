a878eb251c60747d72365e9d87119d3bfc1672aa
Lobaton2020 <andrespipe021028@gmail.com>
2021-02-27 23:14:22 -0500

    Add information init api

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,29 @@
-const auth = (req,res)=>{
-    res.json({
-        "message":"Bienvenido a esta api de test, 2020-02-25"
-    })
+const auth = (req, res) => {
+    res.send(`
+        <h1>"Bienvenido a esta api de test, 2020-02-25</h1>
+            <h1>USO ENDPOINTS</h1>
+            <h3>Host: https://hostname/</h3>
+            <h2>FILMS</h2>
+            <p>
+            body: title:type.STRING, <br />
+            description: type.STRING, <br />
+            score:type.INTEGER,<br />
+            director:type.STRING<br />
+            </p>
+            <ul>
+            <li>GET: /api/films</li>
+            <li>POST: /api/films</li>
+            <li>GET: /api/films/:id</li>
+            <li>PUT: /api/films/:id</li>
+            <li>DELETE: /api/films/:id</li>
+            </ul>
+            <h2>USERS</h2>
+            <ul>
+            <li>POST: /api/users/login { email, password }</li>
+            <li>POST: /api/users/register { username,email, password }</li>
+            </ul>
+        `
+    )
 }
 
 export { auth }
